75bcd95a115cc579502605d8bbf6853e24727d25
cafed <caffier.ed@gmail.com>
2021-07-02 11:54:12 +0200

    createaccount form

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-
+use App\Entity\Account;
+use App\Form\CreateAccountType;
 
 /**
  * @IsGranted("IS_AUTHENTICATED_FULLY")
@@ -22,6 +24,25 @@ class FrontController extends AbstractController
     {
         return $this->render('front/index.html.twig', [
 
+        ]);
+    }
+
+    /**
+     * @Route("/createAccount", name="createAccount")
+     */
+    public function createAccount(Request $request): Response
+    {
+
+        $account = new Account();
+        $form = $this->createForm(CreateAccountType::class, $account);
+        $form->handleRequest($request);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($account);
+        $entityManager->flush();
+
+        return $this->render('createAccount/createAccount.html.twig',[
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/CreateAccountType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Account;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateAccountType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date_creation')
+            ->add('type')
+            ->add('amount')
+            ->add('account_number')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Account::class,
+        ]);
+    }
+}
