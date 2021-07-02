cc2f34f479d81927e3c7a0b5778b60f926799120
cafed <caffier.ed@gmail.com>
2021-07-02 12:03:35 +0200

    solve conflict front controller

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use App\Entity\Account;
 use App\Form\CreateAccountType;
 
 /**
