e51828991196e4b6a18106016a2b4bdb849ed796
taoufik35 <79227708+taoufik35@users.noreply.github.com>
2021-07-05 14:21:13 +0200

    indexpage

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,23 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Operation;
-use App\Form\OperationType;
-use App\Form\CreateAccountType;
 use App\Entity\Account;
 use App\Entity\User;
 use App\Entity\Operation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-<<<<<<< HEAD
 use App\Repository\AccountRepository;
 use App\Repository\OperationRepository;
-=======
-use Symfony\Component\HttpFoundation\Request;
->>>>>>> 39604ce3975955cce44364503a6909599dc4c126
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-
+use App\Form\CreateAccountType;
 
 /**
  * @IsGranted("IS_AUTHENTICATED_FULLY")
@@ -32,8 +26,7 @@ class FrontController extends AbstractController
      */
     public function index(): Response
     {
-        $accountRepository = $this->getDoctrine()->getRepository(Account::class);
-        $accounts = $accountRepository->findAll();
+        $accounts = $this->getUser()->getAccounts();
 
         return $this->render('front/index.html.twig', [
             'accounts' => $accounts,
@@ -54,14 +47,12 @@ class FrontController extends AbstractController
             $account->setDateCreation(new \DateTime());
             $account->setType($account->getType());
             $account->setAmount($account->getAmount());
-            $account->setAccountNumber(random_int(1111111, 9999999));
+            $account->setAccountNumber($account->getAccountNumber());
             $account->setUser($this->getUser());
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($account);
             $entityManager->flush();
-            
-            return $this->redirectToRoute('index');
         }   
 
 
@@ -70,7 +61,6 @@ class FrontController extends AbstractController
         ]);
     }
 
-<<<<<<< HEAD
 
 
     /**
@@ -86,31 +76,6 @@ class FrontController extends AbstractController
         return $this->render('front/single.html.twig', [
             'account' => $account,
             'operation'=> $operation,
-=======
-    /**
-     * @Route("/operation", name="operation")
-     */
-    public function addoperation(Request $request): Response
-    {
-        $operation = new Operation();
-        $form = $this->createForm(OperationType::class, $operation);
-        // On traite les données soumises lors de la requêtes dans l'objet form
-        $form->handleRequest($request);
-        // Si on a soumis un formulaire et que tout est OK
-        if($form->isSubmitted() && $form->isValid()) {
-            $operation->setDateOperation(new \DateTime());
-            // On enregistre le nouveau sujet
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($operation);
-            // Attention les requêtes ne sont exécutées que lors du flush donc à ne pas oublier
-            $entityManager->flush();
-
-            return $this->redirectToRoute('index');
-            
-        }
-        return $this->render('front/operation.html.twig', [
-            "form" => $form->createView() 
->>>>>>> 39604ce3975955cce44364503a6909599dc4c126
         ]);
     }
 }
